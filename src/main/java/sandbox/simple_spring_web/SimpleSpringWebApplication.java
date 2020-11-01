0334b638e023c9a5af7b3fb623210439177adf30
policeman-kh <policeman_kh@msn.com>
2020-11-01 12:13:08 +0900

    Enable h2c and metrics

--- src/main/java/sandbox/simple_spring_web/SimpleSpringWebApplication.java
+++ src/main/java/sandbox/simple_spring_web/SimpleSpringWebApplication.java
@@ -1,13 +1,21 @@
 package sandbox.simple_spring_web;
 
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleSpringWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringWebApplication.class, args);
+    }
 
+    // To use h2c.
+    @Bean
+    public TomcatConnectorCustomizer connectorCustomizer() {
+        return (connector) -> connector.addUpgradeProtocol(new Http2Protocol());
+    }
 }
